a693bac5330868bf0199fab46842311a68ad94c2
Arun Kumaraswamy <arunkumaraswamy@Aruns-MacBook-Air.local>
2024-07-06 00:30:29 +0530

    Updated the repo

--- performancetesting/src/db/createDB.js
+++ performancetesting/src/db/createDB.js
@@ -2,12 +2,9 @@ const Influx = require('influx');
 const influx = new Influx.InfluxDB({
     host: 'localhost',
     port: 8087,
-    username: 'username',
-    password: 'password'
+    username: '<username>',
+    password: '<password>'
 })
 const dbName = 'QACommunity'
 // Create an InfluxDB database
 influx.createDatabase(dbName);
-console.log(dbName);
-//influx.getMeasurements(dbName).then(names =>    console.log('My measurement names are: ' + names.join(', ')));
-
